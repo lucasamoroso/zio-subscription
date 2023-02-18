daed19e65f4245fd1ff85e97b042b9d6dd5fced6
lucasamoroso <amorosolucas@gmail.com>
2023-02-17 21:37:53 -0300

    add retry

--- src/main/scala/kafka/SubscriptionsProducer.scala
+++ src/main/scala/kafka/SubscriptionsProducer.scala
@@ -1,27 +1,39 @@
 package com.lamoroso.example
 package kafka
 
-import zio.ZIO
-import zio.ZLayer
+import zio.*
+import zio.durationInt
 import zio.kafka.producer.Producer
 import zio.kafka.producer.ProducerSettings
 import zio.kafka.serde.Serde
 
+import scala.util.control.NonFatal
+
 import com.lamoroso.example.config.AppConfig
 import com.lamoroso.example.model.Subscription
 import model.error.ServiceError.DatabaseError
+import org.apache.kafka.clients.producer.RecordMetadata
 
 final case class SubscriptionsProducer(config: AppConfig, producer: Producer):
-  def notify(subscription: Subscription) =
+  def notify(subscription: Subscription): ZIO[Any, DatabaseError, RecordMetadata] =
     ZIO.logInfo(s"Notifying change in subscription ${subscription.id}") *>
       producer
-        .produce[Any, String, Subscription](
+        .produceAsync[Any, String, Subscription](
           topic = config.kafka.subscriptionTopic,
           key = subscription.id.toString(),
           value = subscription,
           keySerializer = Serde.string,
           valueSerializer = SubscriptionSerializer.serializer
         )
+        .flatMap(task =>
+          task.logError(s"There was an error producing message from subscription ${subscription.id}. Retrying...") *>
+            // If we don't receive an acknowledgement from the broker for the transmission of the record we'll retry it, we should be more careful about the errors being refined because maybe there are only a couple of them that we want retry
+            task.unrefine { case NonFatal(_) =>
+              notify(subscription)
+            }
+        )
+        //If there was an error enqueueing  the record to the Producer's internal buffer retry up to 5 times
+        .retry(Schedule.exponential(10.millis) && Schedule.recurs(5))
         .mapError(_ => DatabaseError()) // TODO: Add new Error type
 
 object SubscriptionsProducer:
